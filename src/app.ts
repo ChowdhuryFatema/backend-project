7267209ede321ec91a27adbf483a516990da8f36
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2024-11-19 01:27:40 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express"
+import cors from "cors"
+import express from'express'
+
+const app = express()
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+export default app;
+
