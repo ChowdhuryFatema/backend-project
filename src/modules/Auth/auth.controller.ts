ca49be43aca9c268ff09b81885e1096e38d5d884
ChowdhuryFatema <fatema7chowdhury.ctg@gmail.com>
2024-12-20 10:52:11 +0600

    Added refresh token in cookie

--- src/modules/Auth/auth.controller.ts
+++ src/modules/Auth/auth.controller.ts
@@ -3,20 +3,31 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AuthServices } from './auth.service';
 import { Request, Response } from 'express';
+import config from '../../config';
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthServices.loginUser(req.body);
+  const { refreshToken, accessToken, needsPasswordChange } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+  });
   sendResponse(res, {
     statusCode: StatusCodes.OK,
     success: true,
     message: 'User is logged in successfully',
-    data: result,
+    data: {
+      accessToken,
+      // refreshToken,
+      needsPasswordChange,
+    },
   });
 });
 
 const changePassword = catchAsync(async (req: Request, res: Response) => {
-  console.log("asdf", req.user, req.body);
-  const {...passwordData} = req.body;
+  console.log('asdf', req.user, req.body);
+  const { ...passwordData } = req.body;
   const result = await AuthServices.changePassword(req.user, passwordData);
   sendResponse(res, {
     statusCode: StatusCodes.OK,
